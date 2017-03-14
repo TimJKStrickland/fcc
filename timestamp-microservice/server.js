a8d61c166f961f918fee21fb1daf18f24c608b94
Tim JK <timjkstrickland@gmail.com>
2017-03-13 23:11:09 -0700

    timestamp service

---
+++ timestamp-microservice/server.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var express = require('express');
+var moment = require('moment');
+var path = require('path');
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.get('/', (req,res)=>{
+  res.send("Home");
+});
+
+app.get('/:timestamp', (req, res)=> {
+  var timestamp = req.params.timestamp;
+  var validatedTime;
+  if(/^[0-9]*$/.test(timestamp)){
+    validatedTime = moment(timestamp, "X");
+    console.log("unix: " + validatedTime);
+  } else {
+    validatedTime = moment(timestamp).format("MMMM DD YYYY");
+  }
+  
+  if(moment(validatedTime).isValid()){
+    var natTime = moment(validatedTime).format("MMMM D, YYYY");
+    var unixTime = moment(validatedTime).format("X");
+    var output ={"unix":unixTime, "natural":natTime};
+  } else {
+    output = {"unix": null, "natural": null};
+  }
+  res.send(output);
+  });
+
+
+
+server.listen(3000, function(){
+  var addr = server.address();
+  console.log("Listening at " + 3000);
+});
+
